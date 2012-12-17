37cf16b08a7f068dafbee0b09664a53fe90f8cc7
Jesse Boyes <jboyes@gilt.com>
2012-12-16 23:11:31 -0500

    Fix auth, stub tableview

--- SoundCloudDemo/SoundCloudDemo/ViewController.m
+++ SoundCloudDemo/SoundCloudDemo/ViewController.m
@@ -20,7 +20,11 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-	// Do any additional setup after loading the view, typically from a nib.
+
+    trackTable = [[UITableView alloc] initWithFrame:self.view.bounds];
+    trackTable.dataSource = self;
+    trackTable.delegate = self;
+    [self.view addSubview:trackTable];
 }
 
 - (void)didReceiveMemoryWarning
@@ -32,8 +36,34 @@
 - (void)viewDidAppear:(BOOL)animated
 {
     [super viewDidAppear:animated];
-    if (![SCSoundCloud account]) {
+
+    SCAccount *account = [SCSoundCloud account];
+
+    if (!account) {
         [self login];
+    } else {
+        NSString *resourceURL = @"https://api.soundcloud.com/me/favorites.json";
+        [SCRequest performMethod:SCRequestMethodGET
+                      onResource:[NSURL URLWithString:resourceURL]
+                 usingParameters:nil
+                     withAccount:account
+          sendingProgressHandler:nil
+                 responseHandler:^(NSURLResponse *response, NSData *data, NSError *error) {
+                     NSError *jsonError = nil;
+                     NSJSONSerialization *jsonResponse = [NSJSONSerialization
+                                                          JSONObjectWithData:data
+                                                          options:0
+                                                          error:&jsonError];
+                     if (!jsonError && [jsonResponse isKindOfClass:[NSArray class]]) {
+                         NSLog(@"Success! %@", jsonResponse);
+                         tracks = (NSArray *)jsonResponse;
+                         [trackTable reloadData];
+                     } else {
+                         NSLog(@"Error decoding JSON response: %@ / Data: %@", jsonError, jsonResponse);
+                     }
+
+                 }];
+
     }
 }
 
@@ -76,6 +106,30 @@
     }];
 }
 
+#pragma mark - UITableViewDelegate and UITableViewDataSource methods
+
+#define kCellIdent @"SCTrackCell"
+
+- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView
+{
+    return 1;
+}
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
+{
+    return [tracks count];
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:kCellIdent];
+    if (!cell) {
+        cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleDefault reuseIdentifier:kCellIdent];
+    }
+
+    return cell;
+}
 
 
 @end
